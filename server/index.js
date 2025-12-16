b95e0647de77c484360b996ef9fc06f291ee3743
thulitha0109 <thulitha0109@gmail.com>
2025-12-16 22:19:27 +0530

    express wildcard route

--- server/index.js
+++ server/index.js
@@ -88,7 +88,7 @@ app.post('/api/send', async (req, res) => {
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../dist')));
 
-    app.get('*', (req, res) => {
+    app.get(/(.*)/, (req, res) => {
         res.sendFile(path.join(__dirname, '../dist', 'index.html'));
     });
 }
